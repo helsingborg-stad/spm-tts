e15ee746279a150d6f540f6d05ec9e63dc7e534f
Tomas Green <tomas.green@gmail.com>
2021-10-22 16:04:24 +0200

    Added some basic tests

--- Tests/TTSTests/TTSTests.swift
+++ Tests/TTSTests/TTSTests.swift
@@ -1,11 +1,82 @@
 import XCTest
-    @testable import TTS
+import AudioSwitchboard
+import Combine
+@testable import TTS
 
-    final class TTSTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-           // XCTAssertEqual(TTS().text, "Hello, World!")
-        }
+
+var switchBoard = AudioSwitchboard()
+final class TTSTests: XCTestCase {
+    var cancellables = Set<AnyCancellable>()
+    let tts = TTS(AppleTTS(audioSwitchBoard: switchBoard))
+    func testFinished() {
+        let expectation = XCTestExpectation(description: "testFinished")
+        let u = TTSUtterance.init("Hej", voice: TTSVoice.init(locale: Locale(identifier: "sv-SE")))
+        var statuses:[TTSUtteranceStatus] = [.none,.queued,.preparing,.speaking,.finished]
+        u.statusPublisher.sink { status in
+            statuses.removeAll { $0 == status }
+            if status == .finished {
+                XCTAssert(statuses.count == 0)
+                expectation.fulfill()
+            }
+        }.store(in: &cancellables)
+        tts.play(u)
+        wait(for: [expectation], timeout: 20.0)
     }
+    func testFailure() {
+        let expectation = XCTestExpectation(description: "testFailure")
+        let u = TTSUtterance.init("Hej", voice: TTSVoice.init(locale: Locale(identifier: "hr-HR")))
+        var statuses:[TTSUtteranceStatus] = [.none,.queued,.preparing,.failed]
+        u.failurePublisher.sink { error in
+            expectation.fulfill()
+            XCTAssert(statuses.count == 0)
+        }.store(in: &cancellables)
+        
+        u.statusPublisher.sink { status in
+            statuses.removeAll { $0 == status }
+        }.store(in: &cancellables)
+        tts.play(u)
+        wait(for: [expectation], timeout: 20.0)
+    }
+    func testCancelled() {
+        let expectation = XCTestExpectation(description: "testCancelled")
+        let tts = TTS(AppleTTS(audioSwitchBoard: switchBoard))
+        let u = TTSUtterance.init("Hej", voice: TTSVoice.init(locale: Locale(identifier: "sv-SE")))
+        var statuses:[TTSUtteranceStatus] = [.none,.queued,.preparing,.speaking,.cancelled]
+        u.statusPublisher.sink { status in
+            statuses.removeAll { $0 == status }
+            if status == .speaking {
+                tts.cancel(u)
+            }
+            if status == .cancelled {
+                XCTAssert(statuses.count == 0)
+                expectation.fulfill()
+            }
+        }.store(in: &cancellables)
+        tts.play(u)
+        wait(for: [expectation], timeout: 20.0)
+    }
+    func testWordBoundary() {
+        let expectation = XCTestExpectation(description: "testCancelled")
+        let tts = TTS(AppleTTS(audioSwitchBoard: switchBoard))
+        let string = "Hello world"
+        let u = TTSUtterance.init(string, voice: TTSVoice.init(locale: Locale(identifier: "en-US")))
+        var words = string.split(separator: " ").map { String($0) }
+        u.wordBoundaryPublisher.sink { boundary in
+            XCTAssert(string[boundary.range] == boundary.string)
+            words.removeAll { $0 == boundary.string }
+        }.store(in: &cancellables)
+        u.statusPublisher.sink { status in
+            if status == .finished {
+                XCTAssert(words.count == 0)
+                expectation.fulfill()
+            }
+        }.store(in: &cancellables)
+        
+        tts.play(u)
+        wait(for: [expectation], timeout: 20.0)
+    }
+    func testAppleSupport() {
+        XCTAssertTrue(AppleTTS.hasSupportFor(locale: Locale(identifier: "sv-SE")))
+        XCTAssertFalse(AppleTTS.hasSupportFor(locale: Locale(identifier: "hr-HR")))
+    }
+}
